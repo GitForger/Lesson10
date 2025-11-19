a01016029792e02f575b448b602ce4688fbafa1b
GitForger <trueforgefather@gmail.com>
2025-11-20 00:53:51 +0700

    Update __init__.py

--- Pages/__init__.py
+++ Pages/__init__.py
@@ -1,6 +1,1 @@
-from LoginPage import LoginPage
-from ProductPage import ProductsPage
-from CartPage import CartPage
-from CheckPage import CheckoutPage
 
-__all__ = ["LoginPage", "ProductsPage", "CartPage", "CheckoutPage"]
